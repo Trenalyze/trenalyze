7564508f63c913a9c5f01dcc70a06ee1172f01c2
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-21 23:45:32 +0100

    new: allow user to turn on or off the debug option

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
+/* It's a Node.js module that allows you to make HTTPS requests. */
 const https = require('https');
 
 
+/* It's a class that sends messages to a user on Trenalyze.com. */
 class Trenalyze {
-    constructor(token, sender) {
-        this.hostname = 'api.trenalyze.com';
+    /**
+     * The constructor function is a special function that is called when an object is created from a
+     * class.
+     * @param token - The token you received from Trenalyze.
+     * @param sender - The whatsapp phone number of the sender with country code.
+     * @param debug - true/false
+     */
+    constructor(token, sender, debug) {
         this.token = token;
         this.sender = sender;
+        this.debug = debug || false;
     }
 
-    setconfig(token, sender) {
+    /**
+     * The setconfig() function returns an object with two properties: hostname and appurl.
+     * @returns An object with two properties.
+     */
+    setconfig() {
         return {
-            token: token,
-            sender: sender,
+            hostname: 'api.trenalyze.com',
             appurl: 'https://trenalyze.com'
         }
     }
 
+    /* It's a function that sends a message to a user on Trenalyze.com. */
     sendMessage(details, result) {
         return this._request('POST', {
             receiver: details.receiver,
@@ -31,10 +44,17 @@ class Trenalyze {
         });
     }
 
+    /**
+     * The _request() function is a private function that makes a request to Trenalyze.com.
+     * @param method - The HTTP method to use.
+     * @param data - The data to send to the server.
+     * @param result - The callback function that will be called when the request is complete.
+     * @returns The result of the request.
+     */
     _request(method, data, result) {
         const options = {
             'method': method,
-            'hostname': this.hostname,
+            'hostname': this.setconfig().hostname,
             'port': 443,
             'path': '/send',
             'headers': {
@@ -47,6 +67,9 @@ class Trenalyze {
 
                 res.on("data", (chunk) => {
                     chunks.push(chunk);
+                    if (this.debug) {
+                        process.stdout.write(chunk);
+                    }
                     result(null, res);
                     return;
                 });
@@ -69,4 +92,5 @@ class Trenalyze {
     }
 }
 
+/* It's exporting the Trenalyze class so that it can be used in other files. */
 module.exports = Trenalyze;
